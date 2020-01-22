b32c5a56442c503214fb002a368ff1bf0f16c660
laura <lauradondiego@gmail.com>
2020-01-22 17:17:11 -0500

    some code down for social graph

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+import math
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -40,13 +45,30 @@ class SocialGraph:
         """
         # Reset graph
         self.last_id = 0
+        # last_id is the num on the outside (1, 2, 3, 4, 5, etc)
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
+        for i in range(0, num_users):  # loop thru users
+            self.add_user(i)  # add each user
         # Add users
-
+        possible_friendships = []
         # Create friendships
+        for userID in self.users:
+            # add 1 to look next to it
+            for friendID in range(userID + 1, self.last_id + 1):
+                # append to possible_friendships array and add USERID and FRIENDID
+                # creates the empty SET()
+                possible_friendships.append((userID, friendID))
+        random.shuffle(possible_friendships)  # built in python function
+        # .floor rounds down a float
+        for i in range(0, math.floor(num_users * avg_friendships / 2)):
+            # ^ finds the average
+            friendships = possible_friendships[i] # [i] is index as you're looping thru
+            self.add_friendship(friendships[0],friendships[1]) # goes into array and gets first index
+            # and then the second index in friendships array (which is a list of random possible friendships)
+
+
 
     def get_all_social_paths(self, user_id):
         """

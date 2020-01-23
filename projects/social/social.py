8c17bc950a4ca2d47a72d916445489ee1e6bcbeb
laura <lauradondiego@gmail.com>
2020-01-23 10:56:30 -0500

    completed social graph

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,39 @@
 import random
 import math
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
 
 class User:
     def __init__(self, name):
@@ -11,7 +44,7 @@ class SocialGraph:
     def __init__(self):
         self.last_id = 0
         self.users = {}
-        self.friendships = {}
+        self.friendships = {} 
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -30,8 +63,11 @@ class SocialGraph:
         Create a new user with a sequential integer ID
         """
         self.last_id += 1  # automatically increment the ID to assign the new user
-        self.users[self.last_id] = User(name)
+        self.users[self.last_id] = User(name) #User from class User ^
+        #self.users is the dict then mapping ID to username (id 1-10)
         self.friendships[self.last_id] = set()
+        # friendships also a dict {nums inside here 3, 4, 6,}
+        # mapping the ID to the friendship
 
     def populate_graph(self, num_users, avg_friendships):
         """
@@ -80,13 +116,42 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
+        # 1:[1] currently in visited
+        # 3:[1, 3] will be in visited too if you pass in 3
         # !!!! IMPLEMENT ME
+        q = Queue()
+        # import queue above
+        q.enqueue([user_id]) # pass in as a list 
+        # path = [] # will be a dict
+        # print("size:", q.size())
+        while q.size() >= 1:
+            # print("size:", q.size())
+            path = q.dequeue()
+            # print("path", path)
+            current_user = path[-1] # CU will be last index of that path
+            # first item added to the list, is also the first one removed
+            if current_user not in visited: # if you haven't been there yet 
+                # path.append(current_user)
+                visited[current_user] = path.copy() #built in python method
+                # if not, add current user to visited dictions, value will be path
+                # print("self.friendships", self.friendships)
+                # print("currentuser:", current_user)
+                # print("Q:", q.queue)
+                # print("wholething:", self.friendships[current_user])
+                for friend in self.friendships[current_user]:
+                    newpath = path.copy()
+                    newpath.append(friend)
+                    # print("path2", path)
+                    q.enqueue(newpath)
+                    # enqueue is like append
+
+        #   friendships: {1: {3, 7}, 2: {8, 9}, 3: {1, 10}, 4: {10}, 5: {6, 7}, 6: {5}, 7: {8, 1, 5}, 8: {9, 2, 7}, 9: {8, 2}, 10: {3, 4}}          
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
-    print(sg.friendships)
+    print("friendships:", sg.friendships)
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print("connections:", connections)
